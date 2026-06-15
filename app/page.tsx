8bc315bb002879f743f2add6489e43a96dc2f01e
mustafa-workspace <mjofficialdeveloper@gmail.com>
2026-06-15 03:56:30 +0300

    change the style about card feed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 
 export default function Home() {
   return (
-    <main className="bg-black" style={{ width: "100vw", height: "100vh" }}>
-      <div className="flex items-center justify-center h-full">
-        <h1 className="text-white text-4xl font-bold">Cognify app</h1>
+    <main className="bg-black">
+      <div className="test-q card p-2 text-center mt-5">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla fuga saepe consequuntur officia illum cum quia, ducimus quisquam qui eveniet?.</p>
       </div>
     </main>
   );
