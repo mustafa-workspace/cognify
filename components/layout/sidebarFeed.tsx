a0fa29f669da903f7ede21e822d6c96af1f6ec07
mustafa-workspace <mjofficialdeveloper@gmail.com>
2026-06-20 19:47:35 +0300

    Design a Links Card

--- components/layout/sidebarFeed.tsx
+++ components/layout/sidebarFeed.tsx
@@ -1,9 +1,11 @@
 import FeedProfile from "@/features/Feeds/Sidebar/components/feedProfile";
+import SidebarFeedLinks from "@/features/Feeds/Sidebar/components/SidebarFeedLinks";
 
 export default function leftSidebar() {
     return (
         <div className="py-6 pl-5 sticky top-15">
             <FeedProfile />
+            <SidebarFeedLinks />
         </div>
     );
 }
